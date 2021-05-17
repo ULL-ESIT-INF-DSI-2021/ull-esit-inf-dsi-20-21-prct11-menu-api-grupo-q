f07a8da5499aad5a10a5672cd23f027923fe9a4f
borjaguanchesicilia <bg.sicilia@gmail.com>
2021-05-17 18:50:01 +0100

    Servidor express

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,16 @@ import * as express from 'express';
 const app = express();
 
 app.get('', (_, res) => {
-  res.send('Hello World!');
+  res.send('Hello World');
+});
+
+app.get('/ingredientes', (_, res) => {
+  //res.send({hola: "holabb", adios: "adiosbb"});
+  res.send('<h1> Bienvenido/a a los ingredientes</h1>  <br><h3>Los ingredientes </h3>');
+});
+
+app.get('/info', (_, res) => {
+    res.send({hola: "holabb", adios: "adiosbb"});
 });
 
 app.listen(3000, () => {
