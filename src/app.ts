dd7e5d0818d158f4419354ac37444b528ad20a33
Adrian Hernandez Suarez <alu0101235516@ull.edu.es>
2021-05-17 20:05:16 +0100

    public directory created

--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,13 @@
 import * as express from 'express';
+import {join} from 'path';
 
 const app = express();
 
-app.get('', (_, res) => {
-  res.send('Hello World');
-});
+// console.log(join(__dirname, '../public')); // sitio por defecto del servidor, lo queremos public.
+console.log();
 
-app.get('/ingredientes', (_, res) => {
-  //res.send({hola: "holabb", adios: "adiosbb"});
-  res.send('<h1> Bienvenido/a a los ingredientes</h1>  <br><h3>Los ingredientes </h3>');
-});
-
-app.get('/information', (_, res) => {
-    res.send({hola: "holabb", adios: "adiosbb"});
-});
-
-app.get('/notes', (_, res) => {
-  res.send({
-    notes: [
-      {
-        title: 'Blue note',
-        body: 'This is a blue note',
-        color: 'blue',
-      },
-      {
-        title: 'Yellow note',
-        body: 'This is a yellow note',
-        color: 'yellow',
-      },
-    ],
-  });
-});
+// marcar el directorio public como sitio por defecto del servidor.
+app.use(express.static(join(__dirname, '../public')));
 
 app.listen(3000, () => {
   console.log('Server is up on port 3000');
