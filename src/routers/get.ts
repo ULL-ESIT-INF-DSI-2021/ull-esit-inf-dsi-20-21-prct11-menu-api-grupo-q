ed230b8e158a5e5fab7ef5fe94e76a8af0eb7314
ACOIDAN MESA HERNANDEZ <ALU0101206479@ULL.EDU.ES>
2021-05-21 16:48:51 +0100

    Platos y ingredientes terminados

--- src/routers/get.ts
+++ src/routers/get.ts
@@ -1,23 +1,60 @@
 import * as express from 'express';
-import {ingredientSchema} from "../models/Ingredients/IngredientSchema";
+import {ingredientSchema} from "../models/Ingredientes/IngredientSchema";
+import { platoSchema } from '../models/Platos/platoSchema';
+import { menuSchema } from '../models/Menu/menuSchema';
 import * as fs from 'fs';
 const path = require('path')
 
+
 export const getRouter = express.Router();
 
-getRouter.get('/ingredientes', (req, res) => {
 
-    const filter = req.query.nombre?{nombre: req.query.nombre.toString()}:{};
+getRouter.get('/ingredients', (req, res) => {
+
+    const filter = req.query.nameIngredient?{nombre: req.query.nameIngredient.toString()}:{};
+
     ingredientSchema.findOne(filter).then((ingredient) => {
       if (ingredient == null) {
-        console.log("Hubo un error")
         res.status(401).send();
       } 
       else {
-          console.log("EL ingrediente es: ", ingredient)
           res.send(ingredient);
       }
     }).catch(() => {
       res.status(400).send();
     });
+});
+
+
+getRouter.get('/courses', (req, res) => {
+
+  const filter = req.query.nameCourse?{nombre: req.query.nameCourse.toString()}:{};
+  console.log(filter)
+  platoSchema.findOne(filter).then((plato) => {
+    if (plato == null) {
+      res.status(401).send();
+    } 
+    else {
+      res.send(plato);
+    }
+  }).catch(() => {
+    res.status(400).send();
+  });
+});
+
+
+getRouter.get('/menus', (req, res) => {
+
+  const filter = req.query.nameMenu?{nameMenu: req.query.nameMenu.toString()}:{};
+
+  menuSchema.findOne(filter).then((menu) => {
+    if (menu == null) {
+      res.status(401).send();
+    } 
+    else {
+        res.send(menu);
+    }
+  }).catch(() => {
+    res.status(400).send();
+  });
 });
--- src/routers/post.ts
+++ src/routers/post.ts
@@ -1,14 +1,16 @@
 import * as express from 'express';
-import {ingredientSchema} from '../models/Ingredients/IngredientSchema';
-import {platoSchema} from '../models/Courses/platoSchema';
+import {ingredientSchema} from '../models/Ingredientes/IngredientSchema';
+import {platoSchema} from '../models/Platos/platoSchema';
 import {menuSchema} from '../models/Menu/menuSchema';
+import { Platos } from '../models/Platos/platos';
+import { Ingrediente } from '../models/Ingredientes/ingredientes';
+import { IngredienteJSON } from '../models/Ingredientes/tiposDefinidos';
 
 
 export const postRouter = express.Router();
 
 
-postRouter.post('/ingredientes', (req, res) => {
-
+postRouter.post('/ingredients', (req, res) => {
   const ingrediente = new ingredientSchema(req.body);
 
   ingrediente.save().then((ingrediente) => {
@@ -18,12 +20,18 @@ postRouter.post('/ingredientes', (req, res) => {
   });
 });
 
-/*
+
 postRouter.post('/courses', (req, res) => {
+  let aux: [Ingrediente, number][] = [];
+  
+  req.body.ingredientes.forEach((ingrediente: [IngredienteJSON, number]) => {
+    let auxIngrediente = new Ingrediente(ingrediente[0].nombre, ingrediente[0].grupo.numGrupo, [ingrediente[0].composicionNutricional.lipidos, ingrediente[0].composicionNutricional.hCarbono, ingrediente[0].composicionNutricional.proteinas, ingrediente[0].composicionNutricional.kCal], [ingrediente[0].localizacion.ciudad, ingrediente[0].localizacion.pais], ingrediente[0].precio);
+    aux.push([auxIngrediente, ingrediente[1]]);
+  });
+  const plato = new Platos(req.body.nombre, aux, req.body.categoria);
+  const platoAIntroducir = new platoSchema(plato);
 
-  const plato = new platoSchema(req.body);
-
-  plato.save().then((plato) => {
+  platoAIntroducir.save().then((plato) => {
     res.status(201).send(plato);
   }).catch((error) => {
     res.status(400).send(error);
@@ -40,4 +48,4 @@ postRouter.post('/menus', (req, res) => {
   }).catch((error) => {
     res.status(400).send(error);
   });
-});*/
+});
