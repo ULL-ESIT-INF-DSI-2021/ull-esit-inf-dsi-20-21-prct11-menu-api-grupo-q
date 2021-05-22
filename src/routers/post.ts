21e8845d21ab67da0e0b4ad4dc6561aabb2ba65f
ACOIDAN MESA HERNANDEZ <ALU0101206479@ULL.EDU.ES>
2021-05-22 17:16:01 +0100

    Añadir platos funcionando

--- src/routers/post.ts
+++ src/routers/post.ts
@@ -25,17 +25,11 @@ postRouter.post('/ingredients', (req, res) => {
 
 
 postRouter.post('/courses', (req, res) => {
-
   let aux: [Ingrediente, number][] = [];
 
-  console.log("Peticion: ", req.body.ingredientes)
-  
   req.body.ingredientes.forEach((ingrediente: [IngredienteJSON, number]) => {
-    console.log(ingrediente);
     const filter = {"nombre": ingrediente[0].nombre};
-    console.log("El filter es: ", filter);
     ingredientSchema.findOne(filter).then((ingredient) => {
-      console.log("El ingrediente es: ", ingredient);
       if (ingredient == null) {
         res.status(401).send({
           error: 'El ingrediente no se encuentra en la base de datos',
@@ -48,37 +42,27 @@ postRouter.post('/courses', (req, res) => {
         let numLipidos = parseFloat(cadena.substring(cadena.indexOf('lipidos:')+9, cadena.indexOf(', hCarbono')));
         let numCarbono = parseFloat(cadena.substring(cadena.indexOf('hCarbono:')+10, cadena.indexOf(', proteinas')));
         let numProteinas = parseFloat(cadena.substring(cadena.indexOf('proteinas:')+11, cadena.indexOf(', kCal')));
-        let numKCal = parseFloat(cadena.substring(cadena.indexOf('kCal:')+6, cadena.indexOf('kCal:')+8));
+        let numKCal = parseFloat(cadena.substring(cadena.indexOf('kCal:')+6, cadena.indexOf('localizacion:')-6));
         let nombreCiudad = cadena.substring(cadena.indexOf('ciudad:')+9, cadena.indexOf('pais')-3);
-        let nombrePais = cadena.substring(cadena.indexOf('pais:')+7, cadena.indexOf('pais:')+13); //revisar salida
+        let nombrePais = cadena.substring(cadena.indexOf('pais:')+7, cadena.indexOf("_id")-7);
         let numPrecio = parseFloat(cadena.substring(cadena.indexOf('precio:')+8));
-        console.log(nombreIngrediente);
-        console.log(numGrupoAlimenticio);
-        console.log(numLipidos);
-        console.log(numCarbono);
-        console.log(numProteinas);
-        console.log(numKCal);
-        console.log(nombreCiudad);
-        console.log(nombrePais);
-        console.log(numPrecio);
         let auxIngrediente = new Ingrediente(nombreIngrediente, numGrupoAlimenticio, [numLipidos, numCarbono, numProteinas, numKCal], [nombreCiudad, nombrePais], numPrecio);
-        console.log("Metemos ingredientes ", [auxIngrediente, ingrediente[1]]);
         aux.push([auxIngrediente, ingrediente[1]]);
+
+        if (req.body.ingredientes.length == aux.length) {
+          const plato = new Platos(req.body.nombre, aux, req.body.categoria);
+          const platoAIntroducir = new platoSchema(plato);
+
+          platoAIntroducir.save().then((plato) => {
+            res.status(201).send(plato);
+          }).catch((error) => {
+            res.status(400).send(error);
+          });
+        }
       }
     }).catch(() => {
       res.status(400).send();
     });
-  });
-
-  //console.log("El conjunto de ingredientes a introducir es: ", aux)
-
-  const plato = new Platos(req.body.nombre, aux, req.body.categoria);
-  const platoAIntroducir = new platoSchema(plato);
-
-  platoAIntroducir.save().then((plato) => {
-    res.status(201).send(plato);
-  }).catch((error) => {
-    res.status(400).send(error);
   });
 });
 
