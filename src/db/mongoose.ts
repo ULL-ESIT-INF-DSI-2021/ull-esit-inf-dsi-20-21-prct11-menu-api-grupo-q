232f1292980cb0a40cdb63f6947c5896bc132709
MaquinaVirtualLinux <alu0101205908@ull.edu.es>
2021-05-23 05:39:23 -0700

    CAMBIO: Fichero mongoose

--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -1,9 +1,12 @@
 import {connect} from 'mongoose';
 
-connect('mongodb://127.0.0.1:27017/BBDD-Información-nutricional', {
+const mongodb_url = process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/BBDD-Información-nutricional';
+
+connect(mongodb_url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
+  useFindAndModify: false,
 }).then(() => {
   console.log('Connection to MongoDB server established');
 }).catch(() => {
